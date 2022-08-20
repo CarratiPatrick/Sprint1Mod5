ea39fe4238243a99c86662d6245cc5aafae4d5ed
PatrickCarrati <patrickferrari.nft@outlook.com>
2022-08-20 16:55:34 -0300

    feat: criando o css do header

--- Sprint1/src/components/Header/Header.jsx
+++ Sprint1/src/components/Header/Header.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import Button from '../Button/Button'
 import Title from '../Title/Title'
+import S from './Header.module.css'
 
 const Header = () => {
   return (
-    <header>
-    <Title text1= "uma seleção de produtos" text2="especial para você " />
+    <header className={S.header}>
+    <Title className={S.titulo} text1= "uma seleção de produtos" text2="especial para você " />
      <p> Todos os produtos desta lista foram selecionados a partir da sua navegação. Aproveite!      </p> 
-    <div>
-    <Button texto="Conheça a Linx" />
-    <Button texto="Ajude o algorítimo" />
-    <Button texto="Seus produtos" />
-    <Button texto="Compartilhe" />
+    <div className={S.divBotao}>
+    <Button css={S.botao} texto="Conheça a Linx" />
+    <Button css={S.botao} texto="Ajude o algorítimo" />
+    <Button css={S.botao} texto="Seus produtos" />
+    <Button css={S.botao} texto="Compartilhe" />
     
     </div>
      </header>
