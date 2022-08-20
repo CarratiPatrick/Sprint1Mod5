dd34cff9dab1090a95d0d234b2ed4f011ec82fcf
PatrickCarrati <patrickferrari.nft@outlook.com>
2022-08-20 16:55:12 -0300

    feat: criando o css do titulo

--- Sprint1/src/components/Title/Title.jsx
+++ Sprint1/src/components/Title/Title.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import S from "./Title.module.css"
 
 const Title = ({text1,text2}) => {
   return (
-    <div>
-      <h2>{text1}</h2>
-      <h1>{text2}</h1>
+    <div className={S.conteinerTitulo}>
+      <h2 className={S.h2}>{text1}</h2>
+      <h1 className={S.h1}>{text2}</h1>
     </div>
   )
 }
