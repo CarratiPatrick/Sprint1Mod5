8da75233b19fad1edf078d08820a7765b6f1ffc6
PatrickCarrati <patrickferrari.nft@outlook.com>
2022-08-20 16:54:43 -0300

    feat: fazendo ajustes no css do footer

--- Sprint1/src/components/Footer/Footer.jsx
+++ Sprint1/src/components/Footer/Footer.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-
+import S from "./Footer.module.css"
 const Footer = () => {
   return (
-    <footer>
-    <p>Testando suas habilidades em HTML,CSS e JS.</p>
-    <p>Linx Impulse</p>
-    <p>2019</p>
-      
+    <footer className={S.footer} >
+    <div className={S.posicao}>
+    <p className={S.texto}>Testando suas habilidades em HTML,CSS e JS.</p>
+    <p className={S.texto}>Linx Impulse</p>
+    <p className={S.texto}>2019</p>
+    </div>
+    
     </footer>
   )
 }
