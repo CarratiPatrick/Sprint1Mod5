d2a68c4bb21254ff21dd09d7cdbd608effb3f21d
PatrickCarrati <patrickferrari.nft@outlook.com>
2022-08-20 14:15:20 -0300

    feat: Card com as divisoes prontos

--- Sprint1/src/components/Card/Card.jsx
+++ Sprint1/src/components/Card/Card.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import Button from "../Button/Button";
 
-const Card = () => {
+const Card = ({ img, nome, texto, valor, desc, parc,ou }) => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <article>
+      <picture>
+        <img src={img} alt="produto" />
+      </picture>
+      <h3>{nome}</h3>
+      <p>{texto}</p>
+      <h4>De:{valor},00</h4>
+      <h5>Por:{desc},00</h5>
+      <p>ou{parc}x de:{ou}</p>
+      <Button texto="Comprar"/>
+    </article>
+  );
+};
 
-export default Card
+export default Card;
