9eb70d4997778df0badbdbf772a9e7155737ee5d
PatrickCarrati <patrickferrari.nft@outlook.com>
2022-08-20 14:29:59 -0300

    feat: header com titulo pronto

--- Sprint1/src/components/Header/Header.jsx
+++ Sprint1/src/components/Header/Header.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import Button from '../Button/Button'
+import Title from '../Title/Title'
 
 const Header = () => {
   return (
+    <header>
+    <Title text1= "uma seleção de produtos" text2="especial para você " />
+     <p> Todos os produtos desta lista foram selecionados a partir da sua navegação. Aproveite!      </p> 
     <div>
-      
+    <Button texto="Conheça a Linx" />
+    <Button texto="Ajude o algorítimo" />
+    <Button texto="Seus produtos" />
+    <Button texto="Compartilhe" />
+    
     </div>
+     </header>
   )
 }
 
---
+++ Sprint1/src/components/Title/Title.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Title = ({text1,text2}) => {
+  return (
+    <div>
+      <h2>{text1}</h2>
+      <h1>{text2}</h1>
+    </div>
+  )
+}
+
+export default Title
