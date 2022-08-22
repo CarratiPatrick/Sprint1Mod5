dc0a83e45a64a04dba48b229add40945f0c55293
PatrickCarrati <patrickferrari.nft@outlook.com>
2022-08-22 20:31:29 -0300

    fix: adiçao do useEffect e ajustes no css

--- Sprint1/src/components/Main/Main.jsx
+++ Sprint1/src/components/Main/Main.jsx
@@ -5,41 +5,60 @@ import { useState } from "react";
 import Card from "../Card/Card";
 import Button from "../Button/Button";
 import FormConvite from "../FormConvite/FormConvite";
-
+import { useEffect } from "react";
+import S from "./Main.module.css";
 const Main = () => {
-  const[info,setInfo]= useState([]);
+  const[produto,setProduto]= useState([]);
   const[pag,setPag]= useState(1);
-  async function handleRequisicao(){
+  async function hendleApi(){
     const response = await fetch(
       `https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=${pag}`
     );
     const json = await response.json();
-    setInfo([...info,...json.products]);
+    setProduto([...produto,...json.products]);
   }
   const proxPag =()=>{
     setPag(pag+1)
   }
+  useEffect(() => {
+    hendleApi();
+  }, [pag]);
+  
   return (
-    <main>
-      <section>
+    <main className={S.main}>
+      <section className={S.primeiraSec}>
         <Texto />
         <Form />
       </section>
-      <section>
+      <section className={S.segundaSec} >
       <div>
       <h3>Sua seleção especial!</h3>
       </div>
-      <div>
-      <Card/>
-      </div>
+      <section className={S.terceiraSec}>
+      {produto.length>0 &&
+        produto.map((prod,index) => {
+          return (
+            <Card 
+              key={index}
+              img={prod.image}
+              nome={prod.name}
+              texto={prod.description}
+              valor={prod.oldPrice}
+              desc={prod.price}
+              parc={prod.installments.count}
+              ou={prod.installments.value}
+            />
+          );
+        })}
+      </section>
       <Button onClick={proxPag} texto="Adquira mais produtos aqui"/>
       </section>
-      <section>
-      <div>
+      <section >
+      <div className={S.quartaSec}>
       <h3>Compartilhe a novidade</h3>
       </div>
       <div>
-      <p>Quer que seus amigos também ganhem a lista personalizada deles?
+      <p className={S.p}>Quer que seus amigos também ganhem a lista personalizada deles?
       Preencha agora!</p>
       <FormConvite/>
       </div>
